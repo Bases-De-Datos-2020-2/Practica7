86b00cb003eb537f645806753c44b43caf3d545d
CarlosAlexisAlvarez <47435890+CarlosAlexisAlvarez@users.noreply.github.com>
2020-05-06 19:22:49 -0500

    Update miembros-DML.sql

--- basesp7/miembros-DML.sql
+++ basesp7/miembros-DML.sql
@@ -1,4 +1,4 @@
 --3
-SELECT first_name, domicilio, 
+SELECT first_name, domicilio, idmiembro
 FROM miembroAsociacion
 WHERE miembroAsociacion.eschofer = true AND miembroAsociacion.esdueno = true AND miembroAsociacion.fechai > 12/31/2019
