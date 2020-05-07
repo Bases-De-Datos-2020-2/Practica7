cc971aab1d16cd4b3673cd460828ee723fd53ed7
David Morales <david_ivan2402@ciencias.unam.mx>
2020-05-06 23:25:05 -0500

    Se agregaron los datos faltantes

---
+++ DML-consultas.sql
@@ -0,0 +1,29 @@
+--1
+SELECT idMiembro, idVehiculo, no_asociacion, nombre
+	fecha_I, domicilio, fotografia, email, no_celular,
+	esDueño, esChofer
+FROM miembroAsociacion, enOperacion
+WHERE miembroAsociacion.idVehiculo = enOperacion.idVehiculoOperacion;
+
+--2
+SELECT*
+FROM vehiculo
+WHERE no_puertas > 2 AND LOWER (marca) = 'nissan' AND refraccion = FALSE;
+
+--3
+SELECT first_name, domicilio, idmiembro
+FROM miembroAsociacion
+WHERE miembroAsociacion.eschofer = true AND miembroAsociacion.esdueno = true AND miembroAsociacion.fechai > 12/31/2019 ;
+-- 4
+SELECT no_cuenta, nombre,instituto From Estudiante
+WHERE instituto != 'Facultad de ciencias', 'FACULTAD DE QUIMICA';
+
+--5
+SELECT idcliente, idvehiculo, destino, tiempo, distancia, nopasajeros
+FROM viaja
+WHERE viaja.nopasajeros >= 2 AND viaja.destino = 'Ciudad Universitaria';
+
+--6
+SELECT idcliente, idvehiculo, destino, tiempo, distancia, nopasajeros
+FROM viaja
+WHERE destino != 'Ciudad Universitaria';
